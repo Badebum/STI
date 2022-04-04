ad529485dcf83d0a68dac53a07f43e857c29f797
Andrey <82231196+Badebum@users.noreply.github.com>
2022-04-04 15:51:50 +0300

    новые правки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 // polici
-// const checkoutSubmitBtn = document.querySelector('.checkout_submit_js');
-// const policiRef = document.querySelector('#polici_chek');
+const checkoutSubmitBtn = document.querySelector('.checkout_submit_js');
+const policiRef = document.querySelector('#polici_chek');
 
-// policiRef.addEventListener('click', policiChecked);
-// function policiChecked() {
-//   if (policiRef.checked) {
-//     checkoutSubmitBtn.classList.remove('disabled');
-//   } else {
-//     checkoutSubmitBtn.classList.add('disabled');
-//   }
-// }
+policiRef.addEventListener('click', policiChecked);
+function policiChecked() {
+  if (policiRef.checked) {
+    checkoutSubmitBtn.classList.remove('disabled');
+  } else {
+    checkoutSubmitBtn.classList.add('disabled');
+  }
+}
 
 // end polici
 
@@ -38,15 +38,20 @@ menuMoblBtnRef.addEventListener('click', () => {
 });
 //
 
-//открытие мини корзины
-const miniBasketRef = document.querySelector('.section_basket_modal');
-const iconBasket = document.querySelector('.basket-js');
-// const closeMiniBasketBtn = document.querySelector('.close_btn_js');
-
-iconBasket.addEventListener('click', onIconBasketClick);
-// closeMiniBasketBtn.addEventListener('click', onIconBasketClick);
-
-function onIconBasketClick() {
-  miniBasketRef.classList.toggle('open');
-}
-//
+const mask = input => {
+  var matrix = '+7 (___) ___-__-__',
+    i = 0,
+    def = matrix.replace(/\D/g, ''),
+    val = input.value.replace(/\D/g, '');
+  if (def.length >= val.length) val = def;
+  input.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+  });
+};
+document.querySelectorAll('.mask__number').forEach(item =>
+  item.addEventListener('click', function () {
+    item.addEventListener('input', mask.bind(null, item), false);
+    item.addEventListener('focus', mask.bind(null, item), false);
+    item.addEventListener('blur', mask.bind(null, item), false);
+  }),
+);
