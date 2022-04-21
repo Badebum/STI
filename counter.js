5705334af59ce0238e483d3b2835e4f143ec9a20
Andrey <82231196+Badebum@users.noreply.github.com>
2022-04-21 17:51:53 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,32 +1,34 @@
-const countProd = document.querySelector('.count-js');
-const plusProd = document.querySelector('.plus-js');
-const minusProd = document.querySelector('.minus-js');
-const curentProdactPrice = document.querySelector('.products-item-inner__price span');
-const amountProdactPrice = document.querySelector('.products-item-inner__amount span');
-const cardProdact = document.querySelector('.products-item-inner');
+window.addEventListener('load', () => {
+  const countProd = document.querySelector('.count-js');
+  const plusProd = document.querySelector('.plus-js');
+  const minusProd = document.querySelector('.minus-js');
+  const curentProdactPrice = document.querySelector('.products-item-inner__price span');
+  const amountProdactPrice = document.querySelector('.products-item-inner__amount span');
+  const cardProdact = document.querySelector('.products-item-inner');
 
-let cunterProdact = Number(countProd.textContent);
-let prodPrice = Number(curentProdactPrice.textContent.replace(/\s/g, ''));
-let prodactTotalCost = prodPrice;
+  let cunterProdact = Number(countProd.textContent);
+  let prodPrice = Number(curentProdactPrice.textContent.replace(/\s/g, ''));
+  let prodactTotalCost = prodPrice;
 
-const onClickPlus = () => {
-  cunterProdact += 1;
-  countProd.textContent = cunterProdact;
-  prodactTotalCost += prodPrice;
-  amountProdactPrice.textContent = prodactTotalCost;
-};
-
-const onClickMinus = () => {
-  if (cunterProdact < 1) {
-    cardProdact.remove();
-  } else {
-    cunterProdact -= 1;
-    prodactTotalCost -= prodPrice;
-    amountProdactPrice.textContent = prodactTotalCost;
+  const onClickPlus = () => {
+    cunterProdact += 1;
     countProd.textContent = cunterProdact;
-  }
-  console.log(cunterProdact);
-};
+    prodactTotalCost += prodPrice;
+    amountProdactPrice.textContent = prodactTotalCost;
+  };
 
-plusProd.addEventListener('click', onClickPlus);
-minusProd.addEventListener('click', onClickMinus);
+  const onClickMinus = () => {
+    if (cunterProdact <= 1) {
+      cardProdact.remove();
+    } else {
+      cunterProdact -= 1;
+      prodactTotalCost -= prodPrice;
+      amountProdactPrice.textContent = prodactTotalCost;
+      countProd.textContent = cunterProdact;
+    }
+    console.log(cunterProdact);
+  };
+
+  plusProd.addEventListener('click', onClickPlus);
+  minusProd.addEventListener('click', onClickMinus);
+});
