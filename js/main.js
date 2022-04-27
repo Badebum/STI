d2564196941c0fbd6414b08fecbeeae0d4d3623d
Andrey <82231196+Badebum@users.noreply.github.com>
2022-04-27 17:33:55 +0300

    копия боевого проекта

--- js/main.js
+++ js/main.js
@@ -1,57 +1,36 @@
-// polici
-const checkoutSubmitBtn = document.querySelector('.checkout_submit_js');
-const policiRef = document.querySelector('#polici_chek');
+window.addEventListener('load', () => {
+  const calcProductСounter = () => {
+    if (document.querySelector('.clio-products-item-inner__price p')) {
+      const arrProductItem = document.querySelectorAll('.clio-products-item');
 
-// policiRef.addEventListener('click', policiChecked);
-function policiChecked() {
-  if (policiRef.checked) {
-    checkoutSubmitBtn.classList.remove('disabled');
-  } else {
-    checkoutSubmitBtn.classList.add('disabled');
-  }
-}
+      arrProductItem.forEach(productItem => {
+        // quantity
+        const isPlus = productItem.querySelector('.clio-plus');
+        const isMinus = productItem.querySelector('.clio-minus');
+        const quantityInputProduct = productItem.querySelector('.clio-count');
+        // prices
+        const curentProductPrice = productItem.querySelector('.clio-products-item-inner__price p');
+        const amountProductPrice = productItem.querySelector('.clio-products-item-inner__amount p');
 
-// end polici
+        let slicePrice = +curentProductPrice.textContent.replace(/\s/g, '');
+        let productTotalCost = slicePrice;
 
-// мобильное меню
-const menuBtnRef = document.querySelector('[data-menu-button]');
-const menuMoblBtnRef = document.querySelector('[data-menu-mobl-button]');
-const mobileMenuRef = document.querySelector('[data-menu]');
-const moblMenuRef = document.querySelector('[data-menu-mobl]');
+        isPlus.addEventListener('click', () => {
+          quantityInputProduct.value++;
+          productTotalCost += slicePrice;
+          amountProductPrice.textContent = productTotalCost;
+        });
 
-menuBtnRef.addEventListener('click', () => {
-  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-  menuBtnRef.classList.toggle('is-open');
-  menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-  mobileMenuRef.classList.toggle('is-open');
+        isMinus.addEventListener('click', () => {
+          if (quantityInputProduct.value <= 1) {
+            return;
+          }
+          quantityInputProduct.value--;
+          productTotalCost -= slicePrice;
+          amountProductPrice.textContent = productTotalCost;
+        });
+      });
+    }
+  };
+  calcProductСounter();
 });
-
-menuMoblBtnRef.addEventListener('click', () => {
-  const expanded_2 = menuMoblBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-  menuMoblBtnRef.classList.toggle('is-open');
-  menuMoblBtnRef.setAttribute('aria-expanded', !expanded_2);
-
-  moblMenuRef.classList.toggle('is-open');
-});
-//
-
-const mask = input => {
-  var matrix = '+7 (___) ___-__-__',
-    i = 0,
-    def = matrix.replace(/\D/g, ''),
-    val = input.value.replace(/\D/g, '');
-  if (def.length >= val.length) val = def;
-  input.value = matrix.replace(/./g, function (a) {
-    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-  });
-};
-document.querySelectorAll('.mask__number').forEach(item =>
-  item.addEventListener('click', function () {
-    item.addEventListener('input', mask.bind(null, item), false);
-    item.addEventListener('focus', mask.bind(null, item), false);
-    item.addEventListener('blur', mask.bind(null, item), false);
-  }),
-);
